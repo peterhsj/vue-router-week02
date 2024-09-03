dde553a95ca21569e56da15938987dd46465332d
PeterHuang <peterhuang@inquartik.com>
2024-09-04 00:53:19 +0800

    feat: add Todo

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -22,7 +22,7 @@
           <v-col cols="12">
             <v-text-field
               type="email"
-              v-model="signInField.email"
+              v-model="setupField.email"
               density="compact"
               label="E-mail"
               variant="solo"
@@ -32,7 +32,7 @@
           <v-col cols="12">
             <v-text-field
               type="password"
-              v-model="signInField.password"
+              v-model="setupField.password"
               density="compact"
               label="Password"
               variant="solo"
@@ -42,7 +42,7 @@
           <v-col cols="12">
             <v-text-field
               type="text"
-              v-model="signInField.nickname"
+              v-model="setupField.nickname"
               density="compact"
               label="Nickname"
               variant="solo"
@@ -123,16 +123,129 @@
   
   <div v-if="user.uid" class="text-center my-5">
     <h2>{{ user.nickname }}，歡迎光臨</h2>    
+    <v-divider></v-divider>
   </div>
 
-  <v-divider></v-divider>
+  <div v-if="tokenCookie">
+    <h2 class="py-3">代辦事項</h2>
+    <div>
+      <v-card>
+        <v-card-text class="bg-surface-light pt-4">
+          <v-container>            
+            <v-row>
+              <v-col cols="4">
+                <v-text-field
+                  type="text"
+                  v-model="addForm.content"
+                  density="compact"
+                  placeholder="請輸入代辦事項"
+                  variant="solo"
+                  hide-details>
+                </v-text-field>
+              </v-col>
+              <v-col cols="2">
+                <v-btn
+                  type="button"
+                  class="text-none"
+                  color="indigo-darken-3"
+                  variant="flat"
+                  @click="addTodo"
+                  block
+                >新增</v-btn>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col cols="12">
+                <v-data-table
+                  :headers="headers"
+                  :items="serverItems"
+                >
+                  <template v-slot:item.actions="{ item }">
+                    <v-btn
+                      class="me-2"
+                      size="small"
+                      @click="editItem(item)"
+                    >
+                      編輯
+                    </v-btn>
+                    <v-btn
+                      size="small"
+                      @click="deleteItem(item)"
+                    >
+                      刪除
+                    </v-btn>
+                  </template>
+                  <template v-slot:no-data>
+                    尚無資料
+                  </template>
+                </v-data-table>
+                <v-dialog
+                  v-model="dialog"
+                  max-width="500px"
+                >                      
+                  <v-card>
+                    <v-card-title>
+                      <span class="text-h5">編輯</span>
+                    </v-card-title>
 
+                    <v-card-text>
+                      <v-container>
+                        <v-row>
+                          <v-col
+                            cols="12"
+                          >
+                            <v-text-field
+                              v-model="editedItem.content"
+                              label="待辦事項"
+                            ></v-text-field>
+                          </v-col>
+                        </v-row>
+                      </v-container>
+                    </v-card-text>
+
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+                      <v-btn
+                        color="blue-darken-1"
+                        variant="text"
+                        @click="close"
+                      >
+                        取消
+                      </v-btn>
+                      <v-btn
+                        color="blue-darken-1"
+                        variant="text"
+                        @click="save"
+                      >
+                        儲存
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>  
+                <v-dialog v-model="dialogDelete" max-width="500px">
+                    <v-card>
+                      <v-card-title class="text-h5">確認刪除 {{ editedItem.content }} 這個項目 ?</v-card-title>
+                      <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue-darken-1" variant="text" @click="closeDelete">取消</v-btn>
+                        <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirm">確認</v-btn>
+                        <v-spacer></v-spacer>
+                      </v-card-actions>
+                    </v-card>
+                  </v-dialog>
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-card-text>
+      </v-card>
+    </div>
+  </div>
 
 </template>
 
 <script setup>
 import axios from 'axios';
-import { onMounted, ref } from "vue";
+import { onMounted, ref, nextTick } from "vue";
 
 const api = 'https://todolist-api.hexschool.io';
 const isRegister = ref(false);
@@ -158,7 +271,7 @@ const signup = async () => {
     uid.value = res.data.uid;
     cleanMessage();
   } catch (err) {
-    errMessage.value = `${err.response.data.message.join('，')}`;
+    errMessage.value = `${err.response.data.message}`;
   }
 };
 
@@ -180,7 +293,7 @@ const signin = async () => {
     // 驗證登入
     checkoutToken();
   } catch (err) {
-    errMessage.value = `${err.response.data.message.join('，')}`;
+    errMessage.value = `${err.response.data.message}`;
   }
 };
 
@@ -189,7 +302,6 @@ const user = ref({});
 // 驗證登入
 const checkoutToken = async () => {
   tokenCookie.value = document.cookie.replace(/(?:(?:^|.*;\s*)customTodoToken\s*\=\s*([^;]*).*$)|^.*$/, "$1",);
-  console.log('登入', tokenCookie.value);
   const res = await axios.get(`${api}/users/checkout`, {
     headers: {
       Authorization: tokenCookie.value,
@@ -198,12 +310,11 @@ const checkoutToken = async () => {
 
   user.value = res.data;
   isLogin.value = false;
+  fetchTodo();
 };
 
 // 登出
 const signOut = async () => {
-  tokenCookie.value = document.cookie.replace(/(?:(?:^|.*;\s*)customTodoToken\s*\=\s*([^;]*).*$)|^.*$/, "$1",);
-  console.log('登出', tokenCookie.value);
   try {
   const res = await axios.post(`${api}/users/sign_out`,{}, {
     headers: {
@@ -218,9 +329,127 @@ const signOut = async () => {
   }
 };
 
+// Add Todo
+const addForm = ref({
+  content: null
+});
+
+const addTodo = async () => {
+  const payload = { ...addForm.value  };
+  try {
+    const res = await axios.post(`${api}/todos`, payload, {
+      headers: {
+        Authorization: tokenCookie.value,
+      }
+    });
+    fetchTodo();
+    addForm.value.content = null;
+  } catch (err) {
+    console.log({err})
+  }
+};
+
+// Todo 列表
+const headers = ref([
+  {
+    title: '待辦事項',
+    align: 'start',
+    sortable: false,
+    key: 'content',
+  },
+  { title: '操作',
+    sortable: false,
+    key: 'actions',
+    align: 'end'
+  },
+]);
+const serverItems = ref([]);
+const editedItem = ref({});
+const dialog = ref(false);
+const dialogDelete = ref(false);
+
+// 取得Todo 列表
+const fetchTodo = async () => {
+  try {
+    const res = await axios.get(`${api}/todos`, {
+      headers: {
+        Authorization: tokenCookie.value,
+      }
+    });
+    serverItems.value = res.data.data;
+  } catch (err) {
+    console.log({err})
+  }
+};
+
+// 編輯 Todo
+const editItem = (item) => {
+  editedItem.value = {...item};
+  dialog.value = true;
+};
+
+// 刪除 Todo
+const deleteItem = (item) => {
+  dialogDelete.value = true;
+  editedItem.value = {...item};
+};
+
+// 確認刪除
+const deleteItemConfirm = async () => {
+  const id = editedItem.value.id;
+  
+  try {
+    const res = await axios.delete(`${api}/todos/${id}`,  {
+      headers: {
+        Authorization: tokenCookie.value,
+      }
+    });
+    fetchTodo();
+    closeDelete();  
+  } catch (err) {
+    console.log({err})
+  }
+};
+
+// 關閉視窗
+const close = () => {
+  dialog.value = false;
+  nextTick(() => {
+    editedItem.value = {};
+  })
+};
+
+// 關閉刪除視窗
+const closeDelete = () => {
+  dialogDelete.value = false;
+  nextTick(() => {
+    editedItem.value = {};
+  })
+};
+
+const save = async () => {
+  const id = editedItem.value.id;
+  const payload = { content: editedItem.value.content };
+  
+  try {
+    const res = await axios.put(`${api}/todos/${id}`, payload, {
+      headers: {
+        Authorization: tokenCookie.value,
+      }
+    });
+    fetchTodo();
+    close();  
+  } catch (err) {
+    console.log({err})
+  }
+
+};
 
 onMounted(() => {
-  checkoutToken();
+  if (tokenCookie.value) {
+    checkoutToken();
+    fetchTodo();
+  }
 })
 
 </script>
